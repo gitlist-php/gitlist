15139b24eef66aeee47482eed1fbfdb1f35458a1
Philippe Bourdin <git@Bourdin.ch>
2017-08-02 12:46:30 +0200

    Forgot to replace one "$request['request']" with "['request_stack']->getCurrentRequest()" necessary for the Silex 2 migration.

--- src/Controller/CommitController.php
+++ src/Controller/CommitController.php
@@ -34,7 +34,7 @@ class CommitController implements ControllerProviderInterface
             list($branch, $file) = $app['util.repository']->extractRef($repository, $branch, $file);
 
             $type = $file ? "$branch -- \"$file\"" : $branch;
-            $pager = $app['util.view']->getPager($app['request']->get('page'), $repository->getTotalCommits($type));
+            $pager = $app['util.view']->getPager($app['request_stack']->getCurrentRequest()->get('page'), $repository->getTotalCommits($type));
             $commits = $repository->getPaginatedCommits($type, $pager['current']);
             $categorized = array();
 
@@ -44,7 +44,7 @@ class CommitController implements ControllerProviderInterface
                 $categorized[$date][] = $commit;
             }
 
-            $template = $app['request']->isXmlHttpRequest() ? 'commits_list.twig' : 'commits.twig';
+            $template = $app['request_stack']->getCurrentRequest()->isXmlHttpRequest() ? 'commits_list.twig' : 'commits.twig';
 
             return $app['twig']->render($template, array(
                 'page'           => 'commits',
