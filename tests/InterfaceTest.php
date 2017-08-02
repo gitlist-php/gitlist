85a79c16774a5767fc5b9a0c94ae9fa80b0701f9
Philippe Bourdin <git@Bourdin.ch>
2017-08-02 12:47:24 +0200

    Test passes now. Thank you all!

--- tests/InterfaceTest.php
+++ tests/InterfaceTest.php
@@ -269,8 +269,6 @@ class InterfaceTest extends WebTestCase
         $client = $this->createClient();
 
         $crawler = $client->request('GET', '/GitTest/commits/master/test.php');
-// temporary debug code:
-var_dump($client->getResponse()->getContent()); die();
         $this->assertTrue($client->getResponse()->isOk());
         $this->assertEquals('Initial commit', $crawler->filter('.table tbody tr td h4')->eq(0)->text());
 
